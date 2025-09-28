9903bb3f16d26e2d5f8960dc9bc6d54166b4a4b1
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-09-28 23:26:03 +0530

    instance install shell roboshop

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0740382b167ac0ce2" # This is Security Group ID
+
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0740382b167ac0ce2 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output text)
+
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+         IP=$(aws ec2 describe-instances --instance-ids i-0272eb5087250ccf6 --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids i-0272eb5087250ccf6 --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+
+    echo "$instance: $IP"
+done

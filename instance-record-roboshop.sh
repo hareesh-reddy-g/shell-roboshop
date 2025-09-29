9da5403990881570a9daeb8bbfa53ce09f1ea66e
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-09-29 20:23:24 +0530

    launch instance create records through scripting

---
+++ instance-record-roboshop.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"  # This is AMI ID
+SG_ID="sg-0740382b167ac0ce2" # This is Security Group ID
+ZONE_ID="Z0278110EQPYQ4AALTQX" # This is Domain or Record Zone ID
+DOMAIN_NAME="hareesh.space"
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+         RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.hareesh.space
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # hareesh.space
+    fi
+
+    echo "$instance: $IP"
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
+done
